ea56b2b1315b6c22fabff94f2cb1228d4ba7f12e
echizenyayota <virtualmisso30@gmail.com>
2024-03-18 16:11:00 +0900

    13:31 Get All Books with mongoose

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { Book } from "./models/bookModels.js";
 
 const app = express();
 
+// Middleware for parsing request body
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -12,13 +13,13 @@ app.get('/', (req, res) => {
   return res.status(200).send('Welcome to MERN Stack tutorial');
 });
 
+// Route to save a new book
 app.post('/books', async (req, res) => {
   try {
     if(
       !req.body.title ||
       !req.body.author ||
       !req.body.publishYear 
-
     ) {
       return res.status(400).send({
         message: 'Send all required fields: title, author, publishYear',
@@ -39,6 +40,21 @@ app.post('/books', async (req, res) => {
     res.status(500).send({ message: error.message});
   }
 });
+
+// Route to get all books from a database
+app.get('/books', async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch(error) {
+    console.log(error.message);
+    res.status(500).send({ message: error.message })
+  }
+});
+
 
 mongoose
   .connect(mongoDBURL)
